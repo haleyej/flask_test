ca4a421176dd0084d936e5d7b4232b22b199d81c
haleyej <haleyej@umich.edu>
2024-09-06 15:08:11 -0400

    added shell context

--- microblog.py
+++ microblog.py
@@ -1,3 +1,14 @@
 #top level script to define a Flask instance
+import sqlalchemy as sa 
+import sqlachemy.orm as so
+from app import app, db
+from app.models import User, Post
 
-from app import app
+# decorator registers functions as shell context fns
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 
+            'so': so, 
+            'db': db, 
+            'User': User, 
+            'Post': Post}
